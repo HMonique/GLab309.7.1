585800323fd7d93be855339165d6fb31a23850ac
HMonique <moniquehl@outlook.com>
2024-09-10 16:52:30 -0400

    first commit

--- src/main/java/com/SpringBootDataJPA/JPA/repository/TutorialRepository.java
+++ src/main/java/com/SpringBootDataJPA/JPA/repository/TutorialRepository.java
@@ -3,11 +3,12 @@ package com.SpringBootDataJPA.JPA.repository;
 
 import com.SpringBootDataJPA.JPA.model.Tutorial;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
-@Repository
+@RepositoryRestResource(path = "learningtutorials")
 public interface TutorialRepository extends JpaRepository<Tutorial, Long>
 {
     List<Tutorial> findByPublished(boolean published);
